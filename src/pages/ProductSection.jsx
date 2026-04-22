3a9213a0c96709e07ad2ca10979a415cfb5f497e
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-04-22 20:00:26 +0500

    28-Updation

--- src/pages/ProductSection.jsx
+++ src/pages/ProductSection.jsx
@@ -5,18 +5,25 @@ import Card from './Card'
 
 function ProductSection() {
   return (
-    <div className='w-full h-auto py-3 bg-[#030712] border'>
-        <div className='max-w-[1200px] w-full border h-auto py-3 mx-auto'>
-            <div className='w-full h-auto py-2 px-2 border-2 flex md:justify-between md:flex-row md:text-start text-center  flex-col justify-center items-center'>
+    <div className='w-full h-auto py-3 bg-[#030712]'>
+        <div className='max-w-[1200px] w-full  h-auto py-3 mx-auto'>
+            <div className='w-full  h-auto py-2 px-2  flex md:justify-between md:flex-row md:text-start text-center  flex-col justify-center items-center'>
                 <div className='leading-[33px]'>
                 <h1 className='text-white text-[35px] font-bold'>Featured Products</h1>
                 <p className='text-gray-300 text-[18px]'>Discover our handpicked selection of trending items</p>
                 </div>
-                <div>
-                    <Button title="View All Products" />
+                <div className='w-full md:w-auto flex justify-center'>
+                    <Button title="View All Products" className='w-full md:w-[190px] max-w-[300px] border-none text-white text-[15px]' />
                 </div>  
             </div>
-            <div className='w-full h-auto border py-3 px-1 flex flex-wrap justify-center gap-3'>
+            <div className='w-full h-auto  py-3 px-1 flex flex-wrap justify-center gap-3'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
                 <Card />
             </div>
         </div>
